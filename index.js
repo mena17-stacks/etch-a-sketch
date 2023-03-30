623e148ebdc3fdb425551d1e5d0142d99549f095
mena17-stacks <mena0811x@gmail.com>
2023-03-30 13:30:24 +0000

    add input for board

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function populateBoard(size){
     let board= document.querySelector('.board');
+    let squares = document.querySelectorAll('div');
+    squares.forEach(dive=> dive.remove); 
     board.style.gridTemplateColumns = `repeat(${size} , 1fr)`; 
     board.style.gridTemplateRows = `repeat(${size} , 1fr)` ;
-
-    for (let i=0; i<256; i++){
+let amount = size * size
+    for (let i=0; i<amount; i++){
         let square = document.createElement('div');
         square.style.backgroundColor='blue';
         board.insertAdjacentElement('beforeend', square);
@@ -11,3 +13,7 @@ function populateBoard(size){
 }
 
 populateBoard(16);
+
+function changeSize(input){
+    populateBoard(input);
+}
