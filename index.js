fd3a461f829fc4fae9697fe35ee9d366cc576a7e
mena17-stacks <mena0811x@gmail.com>
2023-03-30 14:09:36 +0000

    add mouseover

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+let color='black';
+
 function populateBoard(size){
     let board= document.querySelector('.board');
     let squares = document.querySelectorAll('div');
     squares.forEach(dive=> dive.remove); 
     board.style.gridTemplateColumns = `repeat(${size} , 1fr)`; 
     board.style.gridTemplateRows = `repeat(${size} , 1fr)` ;
-let amount = size * size
+    let amount = size * size
     for (let i=0; i<amount; i++){
         let square = document.createElement('div');
-        square.style.backgroundColor='blue';
+        square.addEventListener('mouseover', colorSquare)
+        square.style.backgroundColor='white';
         board.insertAdjacentElement('beforeend', square);
     }
 }
@@ -15,5 +18,18 @@ let amount = size * size
 populateBoard(16);
 
 function changeSize(input){
-    populateBoard(input);
+    if(input>=2 && input<=100){
+        populateBoard(input);
+    }
+    else{
+        alert("error: Please choose a value between 2 and 100.");
+    }
+}
+
+function colorSquare(){
+    this.style.backgroundColor= color;
+}
+
+function changeColor(choice){
+     color= choice; 
 }
